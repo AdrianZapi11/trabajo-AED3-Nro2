50cc247099c0fed30a5af0aa447f8f93f3f9b6aa
AdrianZapi11 <adrizap18@gmail.com>
2025-04-09 19:23:14 -0300

    j

---
+++ src/p6.cpp
@@ -0,0 +1,15 @@
+#include <Arduino.h>
+#include "stm32f103xb.h"
+
+void setup() {
+    RCC->APB2ENR |= (RCC_APB2ENR_IOPAEN_Msk);
+    RCC->APB2ENR |= (RCC_APB2ENR_IOPBEN_Msk);  
+    GPIOA->CRL = 0x44444444;
+    GPIOA->CRH = 0x11111111;
+}
+
+void loop() {
+    uint32_t B = GPIOB->IDR;  
+    uint32_t resultadoAND = B & 0x23; 
+    GPIOA->BSRR = resultadoAND;  
+}
